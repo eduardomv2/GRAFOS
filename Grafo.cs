bf829149b0f2d7956b18ff51f51a21dac49fc485
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2023-11-05 21:23:43 -0600

    Agregar archivos de proyecto.

---
+++ Grafo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Grafo
+{
+    private int numNodos;
+    private List<int>[] listaAdyacencia;
+
+    public Grafo(int numNodos)
+    {
+        this.numNodos = numNodos;
+        listaAdyacencia = new List<int>[numNodos];
+        for (int i = 0; i < numNodos; i++)
+        {
+            listaAdyacencia[i] = new List<int>();
+        }
+    }
+
+    public void AgregarArista(int nodoOrigen, int nodoDestino)
+    {
+        listaAdyacencia[nodoOrigen].Add(nodoDestino);
+        listaAdyacencia[nodoDestino].Add(nodoOrigen);
+    }
+
+    public void MostrarGrafo()
+    {
+        for (int i = 0; i < numNodos; i++)
+        {
+            Console.Write($"Nodo {i}: ");
+            foreach (int nodo in listaAdyacencia[i])
+            {
+                Console.Write($"{nodo} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicación de Grafo No Dirigido");
+            Console.Write("Ingrese el número de nodos: ");
+            int numNodos = int.Parse(Console.ReadLine());
+
+            Grafo grafo = new Grafo(numNodos);
+
+            while (true)
+            {
+                Console.WriteLine("\nSeleccione una opción:");
+                Console.WriteLine("1. Agregar Arista");
+                Console.WriteLine("2. Mostrar Grafo");
+                Console.WriteLine("3. Salir");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese el nodo de origen: ");
+                        int nodoOrigen = int.Parse(Console.ReadLine());
+                        Console.Write("Ingrese el nodo de destino: ");
+                        int nodoDestino = int.Parse(Console.ReadLine());
+                        grafo.AgregarArista(nodoOrigen, nodoDestino);
+                        break;
+                    case 2:
+                        Console.WriteLine("Grafo:");
+                        grafo.MostrarGrafo();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+            }
+        }
+    }
+
